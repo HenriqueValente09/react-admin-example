a596d2ea7afddb99930ce15263be122aef73d16d
HenriqueValente09 <valentedev09@gmail.com>
2023-11-20 18:05:04 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
 import React from 'react'
-import { Component } from 'react';
 import { Admin, Resource} from 'react-admin'
+import crudProvider from 'ra-data-nestjsx-crud'
 import PostList from './components/PostList.js'
 import PostCreate from './components/PostCreate.js'
 import PostEdit from './components/PostEdit.js'
-import buildGraphQLProvider from 'ra-data-graphql-simple';
-import myBuildQuery from './dataProvider.js'
 
-function App() {
-  const [dataProvider, setDataProvider] = React.useState(null);
-    React.useEffect(() => {
-        try {
-        buildGraphQLProvider({ clientOptions: { uri: 'http://localhost:8080/graphql' }, buildQuery: myBuildQuery })
-            .then(graphQlDataProvider => setDataProvider(() => graphQlDataProvider));
-        } catch (error) {
-            console.log(error)
-        }
-    }, []);
-
-    if (!dataProvider) {
-        return <div>Loading </div>;
-    }
-
-    return (
-        <Admin dataProvider= { dataProvider } >
-            <Resource name="Post" list = { PostList } edit = { PostEdit } create = { PostCreate } />
-        </Admin>
-    );
-}
-
+const dataProvider = crudProvider('http://localhost:3001');
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="Post" list={PostList} create={PostCreate} edit={PostEdit} />
+  </Admin>
+);
 export default App;
